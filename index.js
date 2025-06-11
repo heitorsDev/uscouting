29c4cba575e556157e29c87f598ad68d45322b52
heitorsDev <heitorhilles@gmail.com>
2025-06-11 15:50:42 -0300

    app configurado e criação do controller de user

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const pitPerformanceService = require('./services/pitPerformanceService.js')
 const db = require('./database/db')
 const models = require('./models.js')
 const userScoutService = require('./services/userScoutService.js')
+const express = require('express')
+const app = express()
+
+const dotenv = require('dotenv')
+dotenv.config()
+let dbInstance
 
 
 
+app.use(express.json())
+(async () => {
+    dbInstance = await db.openDb()
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
 
-async function main() {
-    const dbInstance = await db.openDb()
-    await dbInstance.exec(`DROP TABLE IF EXISTS users_scout`);
-    await db.createTables(dbInstance)
-    const userScout = new models.userRegister('JohnDoe', 'password123')
-    const createdUserScout = await userScoutService.createUserScout(userScout, dbInstance)
-    console.log('User Scout Created:', createdUserScout)
-    const token = await userScoutService.authUserScout(createdUserScout.name, 'password123', dbInstance)
-    console.log('Auth Token:', token)
-    const decodedUserScout = await userScoutService.jwtTokenVerify(token.token)
-    console.log('Decoded User Scout:', decodedUserScout)
-}
-main()
